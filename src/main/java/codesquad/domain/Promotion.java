ff6fc1a3b0b81ad98fa3e78218085df9e169e00b
Bellroute <kjkun7631@gmail.com>
2019-09-28 02:26:21 +0900

    fet: promotion entity

---
+++ src/main/java/codesquad/domain/Promotion.java
@@ -0,0 +1,33 @@
+package codesquad.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class Promotion {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false, name = "type")
+    private PromotionType type;
+
+    @Column(nullable = false, name = "title")
+    private String title;
+
+    @Column(nullable = false, name = "desc")
+    private String describe;
+
+    @Column(nullable = false, name = "original_price")
+    private int originalPrice;
+
+    @Column(nullable = false, name = "final_price")
+    private int finalPrice;
+}
