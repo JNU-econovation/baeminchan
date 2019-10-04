ce314646a288f803be57c5d596fe57fbc4a095a3
Bellroute <kjkun7631@gmail.com>
2019-10-04 23:11:37 +0900

    fix: travis ci

--- src/main/java/codesquad/BaeminchanApplication.java
+++ src/main/java/codesquad/BaeminchanApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Baeminchan {
+public class BaeminchanApplication {
     public static void main(String[] args) {
-        SpringApplication.run(Baeminchan.class, args);
+        SpringApplication.run(BaeminchanApplication.class, args);
     }
 }
