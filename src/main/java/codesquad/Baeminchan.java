f3c32f7fa9cfd4eee09d6d64622b000ecfab271b
Bellroute <kjkun7631@gmail.com>
2019-10-04 22:52:21 +0900

    fix: springbootapplication class name for travis ci

--- src/main/java/codesquad/Baeminchan.java
+++ src/main/java/codesquad/Baeminchan.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BaeminchanApplication {
+public class Baeminchan {
     public static void main(String[] args) {
-        SpringApplication.run(BaeminchanApplication.class, args);
+        SpringApplication.run(Baeminchan.class, args);
     }
 }
